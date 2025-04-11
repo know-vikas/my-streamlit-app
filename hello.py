bdbf188a53b8290f48ca6264d761e958530f941a
know-vikas <knowvikas24@gmail.com>
2025-04-12 00:37:34 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("hello World")
+print("My name is Vikas Gupta")
+print("My age is 20")
+print("My name is Vikas Gupta. ","My age is 20")
+name="Sita-Ram"
+age=45
+age2=age
+price=45.89
+charset='c'
+print("My name is ",name)
+print(age,charset,price)
+print(age)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(charset))
+age5=23
+old=False
+a=None
+print(type(a))
